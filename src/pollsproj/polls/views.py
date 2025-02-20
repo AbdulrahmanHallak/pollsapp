9f585dd893f4b1fd1670a2d0a49c0b7f583a9d43
Abdulrahman <abdulrahmanhallak04@gmail.com>
2025-02-20 21:30:42 +0300

    even more views

--- src/pollsproj/polls/views.py
+++ src/pollsproj/polls/views.py
@@ -1,8 +1,9 @@
-from asyncio import QueueShutDown
 from django.shortcuts import render, get_object_or_404
-from django.http import Http404, HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.db.models import F
+from django.urls import reverse
 
-from .models import Question
+from .models import Choice, Question
 
 # Create your views here.
 
@@ -21,8 +22,22 @@ def details(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse("you are voting on question %s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])  # type: ignore
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/details.html",
+            {"question": question, "error_message": "you did not select a choice"},
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse("polls:results", args=(question_id,)))
 
 
 def results(request, question_id):
-    return HttpResponse("you are voting on question %s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
