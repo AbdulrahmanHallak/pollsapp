8bb66370e4aae268dc6bfe4b836fa36503ef1db1
Abdulrahman <abdulrahmanhallak04@gmail.com>
2025-02-20 19:59:24 +0300

    add more views

--- src/pollsproj/polls/views.py
+++ src/pollsproj/polls/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from asyncio import QueueShutDown
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404, HttpResponse
+
+from .models import Question
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello, World!")
+    latest_questions = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_questions": latest_questions}
+    return render(request, "polls/index.html", context)
+
+
+def details(request, question_id):
+    question = get_object_or_404(
+        Question.objects.prefetch_related("choice_set"), pk=question_id
+    )
+    return render(request, "polls/details.html", {"question": question})
+
+
+def vote(request, question_id):
+    return HttpResponse("you are voting on question %s" % question_id)
+
+
+def results(request, question_id):
+    return HttpResponse("you are voting on question %s" % question_id)
