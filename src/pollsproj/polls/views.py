b8d4dccf4293b29d5b5c54947e12923aee656ed8
Abdulrahman <abdulrahmanhallak04@gmail.com>
2025-02-20 21:41:24 +0300

    convert to use cbv

--- src/pollsproj/polls/views.py
+++ src/pollsproj/polls/views.py
@@ -1,24 +1,32 @@
+from django.db.models.query import QuerySet
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.db.models import F
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
 
 # Create your views here.
 
 
-def index(request):
-    latest_questions = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_questions": latest_questions}
-    return render(request, "polls/index.html", context)
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_questions"
+
+    def get_queryset(self) -> QuerySet[Question]:
+        """Return the last five published questions"""
+        return Question.objects.order_by("-pup_date")[:5]
 
 
-def details(request, question_id):
-    question = get_object_or_404(
-        Question.objects.prefetch_related("choice_set"), pk=question_id
-    )
-    return render(request, "polls/details.html", {"question": question})
+class DetailsView(generic.DetailView):
+    template_name = "polls/details.html"
+    model = Question
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 
 def vote(request, question_id):
@@ -36,8 +44,3 @@ def vote(request, question_id):
         selected_choice.save()
 
         return HttpResponseRedirect(reverse("polls:results", args=(question_id,)))
-
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
